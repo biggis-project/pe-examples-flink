90aa2b9ea5b4ecdccb846c30abd630bec1c1bd23
Dominik Riemer <riemer@fzi.de>
2018-02-02 10:20:55 +0100

    bug fix in timestamp enricher

--- src/main/java/org/streampipes/examples/flink/processor/timestamp/TimestampController.java
+++ src/main/java/org/streampipes/examples/flink/processor/timestamp/TimestampController.java
@@ -19,7 +19,6 @@ package org.streampipes.examples.flink.processor.timestamp;
 import org.streampipes.examples.flink.config.FlinkConfig;
 import org.streampipes.model.graph.DataProcessorDescription;
 import org.streampipes.model.graph.DataProcessorInvocation;
-import org.streampipes.model.schema.EventProperty;
 import org.streampipes.sdk.builder.ProcessingElementBuilder;
 import org.streampipes.sdk.extractor.ProcessingElementParameterExtractor;
 import org.streampipes.sdk.helpers.EpProperties;
@@ -33,10 +32,9 @@ import org.streampipes.wrapper.flink.FlinkDataProcessorDeclarer;
 import org.streampipes.wrapper.flink.FlinkDataProcessorRuntime;
 import org.streampipes.wrapper.flink.FlinkDeploymentConfig;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TimestampController extends FlinkDataProcessorDeclarer<TimestampParameters> {
+
+  private final String APPEND_PROPERTY = "appendedTime";
 
   @Override
   public DataProcessorDescription declareModel() {
@@ -45,7 +43,7 @@ public class TimestampController extends FlinkDataProcessorDeclarer<TimestampPar
             .iconUrl(FlinkConfig.getIconUrl("enrich-timestamp-icon"))
             .requiredPropertyStream1(EpRequirements.anyProperty())
             .outputStrategy(OutputStrategies.append(
-                    EpProperties.longEp(Labels.empty(), "appendedTime", SO.DateTime)))
+                    EpProperties.longEp(Labels.empty(), APPEND_PROPERTY, SO.DateTime)))
             .supportedProtocols(SupportedProtocols.kafka())
             .supportedFormats(SupportedFormats.jsonFormat())
             .build();
@@ -56,17 +54,9 @@ public class TimestampController extends FlinkDataProcessorDeclarer<TimestampPar
           DataProcessorInvocation graph,
           ProcessingElementParameterExtractor extractor) {
 
-    String appendTimePropertyName = extractor.mappingPropertyValue("appendedTime");
-
-    List<String> selectProperties = new ArrayList<>();
-    for (EventProperty p : graph.getInputStreams().get(0).getEventSchema().getEventProperties()) {
-      selectProperties.add(p.getRuntimeName());
-    }
-
-    TimestampParameters staticParam = new TimestampParameters(
+       TimestampParameters staticParam = new TimestampParameters(
             graph,
-            appendTimePropertyName,
-            selectProperties);
+            APPEND_PROPERTY);
 
     return new TimestampProgram(staticParam, new FlinkDeploymentConfig(FlinkConfig.JAR_FILE,
             FlinkConfig.INSTANCE.getFlinkHost(), FlinkConfig.INSTANCE.getFlinkPort()));
